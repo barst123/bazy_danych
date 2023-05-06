dbfad9a24882c1a38d5f7ea971749cd25550d7c3
barst123 <127050943+barst123@users.noreply.github.com>
2023-05-06 17:33:08 +0200

    Add files via upload

--- 6b.sql
+++ 6b.sql
@@ -3,9 +3,6 @@ USE firma_cw5;
 ALTER TABLE ksiegowosc.pracownicy
 ALTER COLUMN telefon varchar(20);
 
-SELECT *
-FROM ksiegowosc.pracownicy;
-
 UPDATE ksiegowosc.pracownicy
 SET ksiegowosc.pracownicy.telefon='(+48)'+ksiegowosc.pracownicy.telefon
 WHERE ksiegowosc.pracownicy.telefon NOT LIKE '(+48)%';
@@ -21,11 +18,20 @@ SELECT *
 FROM ksiegowosc.pracownicy;
 
 --c
+
+--1 sposob
 SELECT TOP 1 UPPER(id_pracownika) AS id_pracownika, UPPER(imie) AS imie, UPPER(nazwisko) AS nazwisko, UPPER(adres) AS adres, UPPER(telefon) AS telefon
 FROM ksiegowosc.pracownicy
 ORDER BY LEN(nazwisko) DESC;
 
+--2 sposob
+SELECT TOP 1 UPPER(CONCAT(id_pracownika, ', ', nazwisko, ' ', imie, ', ', adres, ', ', telefon)) AS Dane
+FROM ksiegowosc.pracownicy
+ORDER BY LEN(nazwisko) DESC
+
 --d
+
+-- 1 sposob
 SELECT p.id_pracownika, p.imie, p.nazwisko, p.adres, p.telefon, pe.kwota INTO ksiegowosc.dane_md5
 FROM ksiegowosc.pracownicy p
 INNER JOIN ksiegowosc.wynagrodzenie w
@@ -45,6 +51,26 @@ HASHBYTES('md5', m.telefon) AS telefon,
 HASHBYTES('md5', CAST(m.kwota AS varchar)) AS kwota
 FROM ksiegowosc.dane_md5 m;
 
+-- 2 sposob (CTE)
+WITH md_5
+AS
+(
+	SELECT p.id_pracownika, p.imie, p.nazwisko, p.adres, p.telefon, pe.kwota
+	FROM ksiegowosc.pracownicy p
+	INNER JOIN ksiegowosc.wynagrodzenie w
+	ON p.id_pracownika=w.id_pracownika
+	INNER JOIN ksiegowosc.pensje pe
+	ON w.id_pensji=pe.id_pensji
+)
+SELECT 
+HASHBYTES('md5', m.id_pracownika) AS id_pracownika,
+HASHBYTES('md5', m.imie) AS imie,
+HASHBYTES('md5', m.nazwisko) AS nazwisko,
+HASHBYTES('md5', m.adres) AS adres,
+HASHBYTES('md5', m.telefon) AS telefon,
+HASHBYTES('md5', CAST(m.kwota AS varchar)) AS kwota
+FROM md_5 m;
+
 --e
 SELECT p.id_pracownika, p.imie, p.nazwisko, p.adres, p.telefon, pe.kwota AS pensja, pr.kwota AS premia
 FROM ksiegowosc.pracownicy p
@@ -56,6 +82,8 @@ LEFT JOIN ksiegowosc.premie pr
 ON w.id_premii=pr.id_premii;
 
 --f
+
+--1 sposob
 SELECT p.imie, p.nazwisko, pe.kwota AS pensja, ISNULL(pr.kwota, 0) AS premia, g.liczba_godzin, n.liczba_nadgodzin, w.data_ AS data_ INTO ksiegowosc.zapytanie
 FROM ksiegowosc.pracownicy p
 LEFT JOIN ksiegowosc.wynagrodzenie w
@@ -88,5 +116,64 @@ FROM ksiegowosc.zapytanie z;
 SELECT *
 FROM ksiegowosc.raport;
 
-SELECT 'Pracownik '+r.imie+' '+r.nazwisko+', w dniu '+CAST(r.data_ AS varchar)+' otrzyma³ pensjê ca³kowita na kwotê '+CAST(r.pensja_calkowita AS varchar)+', gdzie wynagrodzenie zasadnicze wynosi³o: '+CAST(r.pensja AS varchar)+' z³, premia: '+CAST(r.premia AS varchar)+' z³, nadgodziny: '+CAST(r.nadgodziny AS varchar)+' z³.' AS Raport
+SELECT 
+	'Pracownik '+
+	r.imie+' '+
+	r.nazwisko+
+	', w dniu '+
+	SUBSTRING(CAST(r.data_ AS varchar), 9, 2)+'.'+SUBSTRING(CAST(r.data_ AS varchar), 6, 2)+'.'+SUBSTRING(CAST(r.data_ AS varchar), 1, 4)+
+	' otrzyma³ pensjê ca³kowita na kwotê '+
+	CAST(r.pensja_calkowita AS varchar)+
+	', gdzie wynagrodzenie zasadnicze wynosi³o: '+
+	CAST(r.pensja AS varchar)+
+	' z³, premia: '+
+	CAST(r.premia AS varchar)+
+	' z³, nadgodziny: '+
+	CAST(r.nadgodziny AS varchar)+
+	' z³.' 
+	AS Raport
 FROM ksiegowosc.raport r;
+
+
+--2 sposob (CTE) zastanow sie dlaczego po inner join jest inna kolejnosc niz po left join!!!
+WITH raporcik
+AS
+(
+	SELECT 
+		p.imie AS imie, 
+		p.nazwisko AS nazwisko, 
+		pe.kwota AS pensja, 
+		ISNULL(pr.kwota, 0) AS premia, 
+		g.liczba_godzin AS liczba_godzin, 
+		n.liczba_nadgodzin AS liczba_nadgodzi, 
+		w.data_ AS data_,
+		(pe.kwota/(6*4*liczba_godzin))*n.liczba_nadgodzin AS nadgodziny,
+		pe.kwota+ISNULL(pr.kwota, 0)+(pe.kwota/(6*4*liczba_godzin))*n.liczba_nadgodzin AS pensja_calkowita
+	FROM ksiegowosc.pracownicy p
+	INNER JOIN ksiegowosc.wynagrodzenie w
+	ON p.id_pracownika=w.id_pracownika
+	INNER JOIN ksiegowosc.pensje pe
+	ON w.id_pensji=pe.id_pensji
+	INNER JOIN ksiegowosc.premie pr
+	ON w.id_premii=pr.id_premii
+	INNER JOIN ksiegowosc.nadgodziny2 n
+	ON w.id_pracownika=n.id_pracownika
+	INNER JOIN ksiegowosc.godziny g
+	ON w.id_pracownika=g.id_pracownika
+)
+SELECT 
+	'Pracownik '+
+	r.imie+
+	' '+
+	r.nazwisko+
+	', w dniu '+
+	SUBSTRING(CAST(r.data_ AS varchar), 9, 2)+'.'+SUBSTRING(CAST(r.data_ AS varchar), 6, 2)+'.'+SUBSTRING(CAST(r.data_ AS varchar), 1, 4)+
+	' otrzyma³ pensjê ca³kowita na kwotê '+
+	CAST(r.pensja_calkowita AS varchar)+
+	', gdzie wynagrodzenie zasadnicze wynosi³o: '+
+	CAST(r.pensja AS varchar)+' z³, premia: '+
+	CAST(r.premia AS varchar)+' z³, nadgodziny: '+
+	CAST(r.nadgodziny AS varchar)+
+	' z³.' 
+	AS Raport
+FROM raporcik r;
